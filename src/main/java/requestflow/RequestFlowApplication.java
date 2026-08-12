299a5b3427b9a9411ca22d475ce6bc5d9359912d
Max Solovov <m_solovov1@mail.ru>
2026-08-12 14:39:39 +0300

    TASK-001 initialize Spring Boot project

---
+++ src/main/java/requestflow/RequestFlowApplication.java
@@ -0,0 +1,11 @@
+package requestflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestFlowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RequestFlowApplication.class, args);
+    }
+}
