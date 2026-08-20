b01ef313af2e33e0d7d37bef33ae756dafd6f38a
Max Solovov <m_solovov1@mail.ru>
2026-08-20 11:31:01 +0300

    TASK-003 — Request API V4.0

--- src/main/java/requestflow/request/dto/UpdateRequestDto.java
+++ src/main/java/requestflow/request/dto/UpdateRequestDto.java
@@ -1,6 +1,5 @@
 package requestflow.request.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
